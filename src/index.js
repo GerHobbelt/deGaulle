a8f14fe6aa0fea7925e816e706136992e85d2e59
Ger Hobbelt <ger@hobbelt.com>
2020-04-26 10:37:23 +0200

    added `prettier-standard` package and scripts

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-// 
-// 
+//
+//
+//
+
+var Glob = require('glob').Glob;
+
+var mg = new Glob('**/*.*', {}, function (err, files) {
+  // `files` is an array of filenames.
+  // If the `nonull` option is set, and nothing
+  // was found, then files is ["**/*.js"]
+  // `err` is an error object or null.
+});
